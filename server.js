01039e416045ae5dfca0dcb8b46d5450e5104ae9
Sazzadur Rahman Sujon <rahmansujon94@gmail.com>
2018-09-24 02:12:18 +0600

    add project page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {    
+    res.render('project.hbs', {
+        pageTitle: 'Project Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMsg: 'error 404'
